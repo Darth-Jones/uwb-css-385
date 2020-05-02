676d80839f3fce3076cbaec6a0045fe2baf6db1c
Darth-Jones <55859742+Darth-Jones@users.noreply.github.com>
2020-05-01 18:24:09 -0700

    Add new behaviors
    Mouse movment with M-key Done
    Wayfinding Script Done
    Waypoint Script Done
    Enemy Spawn Script Done

---
+++ hero-game-DJ-JP/Assets/Scripts/EnemySpawn.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawn : MonoBehaviour
+{
+    public int numberOfEnemies = 10;
+
+    public GameObject[] waypoints;
+
+    public GameObject enemySpawn;
+
+    private bool randomWayFinding = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.J))
+            randomWayFinding = !randomWayFinding;
+
+        //Makes sure there are 10 enemies on the screen at all times
+        if (GameObject.FindGameObjectsWithTag("Enemy").Length < 10)
+        {
+            //When a new enemy is spawned
+            GameObject enemy = Instantiate<GameObject>(enemySpawn);
+
+            WayFinding enemyWayFinding = enemy.GetComponent<WayFinding>();
+
+            //will follow the same flight behavior as the others
+            enemyWayFinding.randomWayFinding = randomWayFinding;
+
+            //will have the same list of waypoints
+            enemyWayFinding.waypoints = waypoints;
+
+            //will choose a random waypoint to start at
+            enemyWayFinding.lastWaypoint = Random.Range(0, waypoints.Length);
+
+            //will choose a random start position within the game bounds
+            //Kind of rough would like to improve
+            Vector2 randomVector = new Vector2(Random.Range(-70,70),Random.Range(-40,40));
+            enemy.transform.position = randomVector;
+            
+
+        }
+
+    }
+}
---
+++ hero-game-DJ-JP/Assets/Scripts/WayPoint.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WayPoint : MonoBehaviour
+{
+    public bool visible = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Toggles bool visble
+        if (Input.GetKeyDown(KeyCode.H))
+            visible = !visible;
+
+        SpriteRenderer spritRend = GetComponent<SpriteRenderer>();
+
+        spritRend.enabled = visible;
+    }
+}
